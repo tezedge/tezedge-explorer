ea5e13c1d6114782f3edef97ac19db97760c4cda
Juraj Selep <jurajselep@gmail.com>
2019-09-24 17:30:32 +0200

    Fixed reducers

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,7 +10,7 @@ import { StoreModule } from '@ngrx/store';
 import { EffectsModule } from '@ngrx/effects';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 
-import { reducers, metaReducers } from './reducers';
+import { reducers, metaReducers } from './app.reducers';
 
 import { AppEffects } from './app.effects'
 

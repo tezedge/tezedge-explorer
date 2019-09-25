bad29486a14b97b4536b65936c5cdfe36108ddbf
Juraj Selep <jurajselep@gmail.com>
2019-09-25 03:09:07 +0200

    Added block representation

--- src/app/networking/networking-history/networking-history.component.ts
+++ src/app/networking/networking-history/networking-history.component.ts
@@ -1,4 +1,7 @@
-import { ChangeDetectionStrategy, Component, ElementRef, Input, OnChanges, ViewChild, OnInit } from '@angular/core';
+import {
+  ChangeDetectorRef, ChangeDetectionStrategy, Component,
+  ElementRef, Input, OnChanges, ViewChild, OnInit
+} from '@angular/core';
 import * as d3 from 'd3';
 
 @Component({
@@ -6,16 +9,34 @@ import * as d3 from 'd3';
   templateUrl: './networking-history.component.html',
   styleUrls: ['./networking-history.component.css'],
   changeDetection: ChangeDetectionStrategy.OnPush
-
 })
+
 export class NetworkingHistoryComponent implements OnInit {
 
-  constructor() {
+  constructor(private cd: ChangeDetectorRef) {
   }
 
   ngOnInit() {
 
+    // https://medium.com/netscape/visualizing-data-with-angular-and-d3-209dde784aeb
+    // https://stackoverflow.com/questions/22295644/d3-js-appending-svg-dom-element-not-working
+    var svg = d3.select("#svg")
 
+    // svg.style("fill", "red")
+
+    svg.append("rect")
+      .attr("x", 20)
+      .attr("y", 0)
+      .attr("width", 15)
+      .attr("height", 15);
+
+    svg.append("rect")
+      .attr("x", 40)
+      .attr("y", 0)
+      .attr("width", 15)
+      .attr("height", 15);
+
+    // this.cd.markForCheck();
 
   }
 

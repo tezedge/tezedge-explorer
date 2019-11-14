5f21df9f21987e72d95a3e164bd514783ca95538
Juraj Selep <jurajselep@gmail.com>
2019-11-14 06:17:46 +0100

    Remove graph for desktop

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: true,
   api: {
-    ws: 'ws://127.0.0.1:4927/',
+    // ws: 'ws://127.0.0.1:4927/',
     // ws: 'ws://195.201.111.145:4927/',
-    // ws: 'wss://babylon.tezedge.com',
+    ws: 'wss://babylon.tezedge.com',
   }
 };

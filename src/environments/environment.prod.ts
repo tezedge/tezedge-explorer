b91f66f513873b6a91f538a551870a1a6f83a881
Juraj Selep <jurajselep@gmail.com>
2019-11-14 02:16:41 +0100

    Changed env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,5 +4,5 @@ export const environment = {
     // ws: 'ws://127.0.0.1:4927/',
     // ws: 'ws://195.201.111.145:4927/',
     ws: 'wss://babylon.tezedge.com',
-  } 
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -8,7 +8,7 @@ export const environment = {
     // ws: 'ws://127.0.0.1:4927/',
     // ws: 'ws://195.201.111.145:4927/',
     ws: 'wss://babylon.tezedge.com',
-  } 
+  }
 };
 
 /*

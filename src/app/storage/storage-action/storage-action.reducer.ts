2ded593f14918e1e689d136496ffb4567b247bf9
Juraj Selep <jurajselep@gmail.com>
2019-11-29 16:48:40 +0100

    Added debug

--- src/app/storage/storage-action/storage-action.reducer.ts
+++ src/app/storage/storage-action/storage-action.reducer.ts
@@ -1,10 +1,19 @@
+import { ACTIONS_SUBJECT_PROVIDERS } from '@ngrx/store/src/actions_subject';
+
 const initialState: any = {
     ids: [],
 }
 
+
+function bufferToHex(buffer) {
+    return Array
+        .from(new Uint8Array(buffer))
+        .map(b => b.toString(16).padStart(2, "0"))
+        .join("");
+}
 export function reducer(state = initialState, action) {
     switch (action.type) {
-       
+
         case 'STORAGE_ACTION_LOAD': {
             return {
                 ...state,
@@ -12,9 +21,36 @@ export function reducer(state = initialState, action) {
         }
 
         case 'STORAGE_ACTION_LOAD_SUCCESS': {
+
+            console.log('[STORAGE_ACTION_LOAD_SUCCESS]', action.payload);
             return {
                 ...state,
                 ids: action.payload,
+                entities: action.payload.map(action => {
+
+                    if (action.hasOwnProperty('Set')) {
+
+                        console.log('[action] Set', action.Set.value, new TextDecoder("utf-8").decode(new Uint8Array(action.Set.value)), bufferToHex(new Uint8Array(action.Set.value)) );
+
+                        return {
+                            'Set': {
+                                ...action.Set,
+                                text: new TextDecoder("utf-8").decode(new Uint8Array(action.Set.value)),
+                                hex: bufferToHex(new Uint8Array(action.Set.value))
+                            }
+                        };
+                    }
+
+                    if (action.hasOwnProperty('Delete')) {
+                        return action;
+                    }
+
+                    if (action.hasOwnProperty('RemoveRecord')) {
+                        return action;
+                    }
+
+                    return action;
+                })
             }
         }
 

1c8c5ad5b48f1c8126baec0c4a8ef947e2f26f27
Juraj Selep <jurajselep@gmail.com>
2019-12-04 17:22:58 +0100

    Added draft for action path parsing

--- src/app/storage/storage-action/storage-action.reducer.ts
+++ src/app/storage/storage-action/storage-action.reducer.ts
@@ -1,4 +1,5 @@
 import { ACTIONS_SUBJECT_PROVIDERS } from '@ngrx/store/src/actions_subject';
+import { bindCallback } from 'rxjs';
 
 const initialState: any = {
     ids: [],
@@ -34,9 +35,11 @@ export function reducer(state = initialState, action) {
                         return {
                             Set: {
                                 ...action.Set,
-                                key: action.Set.key.toString().replace(/,/g, '/'),
+                                key: parseKey(action.Set.key),
                                 text: new TextDecoder('utf-8').decode(new Uint8Array(action.Set.value)),
-                                hex: bufferToHex(new Uint8Array(action.Set.value))
+                                hex: bufferToHex(new Uint8Array(action.Set.value)),
+                                category: action.Set.key[0] === 'data' ? action.Set.key[1] : action.Set.key[0],
+                                color: categoryColor(action.Set.key[0] === 'data' ? action.Set.key[1] : action.Set.key[0])
                             }
                         };
                     }
@@ -56,5 +59,74 @@ export function reducer(state = initialState, action) {
 
         default:
             return state;
+    }
+}
+
+export function parseKey(key) {
+
+    // process block priority
+    if ((key.indexOf("block_priority") > 0)) {
+        key = key.filter((value, index) => {
+            return (index > 2) ? true : false;
+        })
+    }
+    // process contract
+    if ((key.indexOf("contracts") > 0) && (key.indexOf("index") > 0)) {
+        key = key.filter((value, index) => {
+            // remove index from path
+            return (index > 8) ? true : false;
+        })
+    }
+
+    // process delegates_with_frozen_balance
+    if ((key.indexOf("delegates_with_frozen_balance") > 0)) {
+        key = key.filter((value, index) => {
+            // remove contract index from path
+            return ((index > 1 && index < 4) || index > 8) ? true : false;
+        })
+    }
+
+    // replace , with / 
+    return key.length > 0 ? '/' + key.toString().replace(/,/g, '/') : '';
+}
+
+export function categoryColor(category) {
+    switch (category) {
+        case 'active_delegates_with_rolls': {
+            return 'red';
+        }
+        case 'big_maps': {
+            return 'green';
+        }
+        case 'block_priority': {
+            return 'green';
+        }
+        case 'commitments': {
+            return 'red';
+        }
+        case 'contracts': {
+            return 'darkblue';
+        }
+        case 'cycle': {
+            return 'darkgreen';
+        }
+        case 'delegates': {
+            return 'red';
+        }
+        case 'delegates_with_frozen_balance': {
+            return 'red';
+        }
+        case 'ramp_up': {
+            return 'yellow';
+        }
+        case 'rolls': {
+            return 'lightblue';
+        }
+        case 'votes': {
+            return 'lightblue';
+        }
+
+        default:
+            return 'black';
     }
 }

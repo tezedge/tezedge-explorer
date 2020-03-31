bb84aae552fa98b91b63338906e50a4038c2e0f9
Juraj Selep <jurajselep@gmail.com>
2020-03-31 08:06:56 +0200

    Updated docker

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,9 +3,9 @@ export const environment = {
   api: {
     // ws: 'ws://127.0.0.1:4927/',
     // // ws: 'ws://195.201.111.145:4927/',
-    http: 'https://babylon.tezedge.com:8732',
-    ws: 'wss://babylon.tezedge.com',
-    //http: 'http://127.0.0.1:18732',
-    //ws: 'ws://127.0.0.1:4927/',
+    // http: 'https://babylon.tezedge.com:8732',
+    // ws: 'wss://babylon.tezedge.com',
+    http: 'http://127.0.0.1:18732',
+    ws: 'ws://127.0.0.1:4927/',
   }
 };

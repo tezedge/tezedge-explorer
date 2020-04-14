820d2422318b49740aa8c377d0c202e481cdac42
Juraj Selep <jurajselep@gmail.com>
2020-04-14 02:06:28 +0200

    Environment change

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,9 @@
 export const environment = {
   production: true,
   api: {
-    // ws: 'ws://127.0.0.1:4927/',
-    // // ws: 'ws://195.201.111.145:4927/',
-    http: 'https://carthage.tezedge.com:8732',
-    ws: 'wss://carthage.tezedge.com',
-    // http: 'http://127.0.0.1:18732',
-    // ws: 'ws://127.0.0.1:4927/',
+    // http: 'https://carthage.tezedge.com:8732',
+    // ws: 'wss://carthage.tezedge.com',
+    http: 'http://127.0.0.1:18732',
+    ws: 'ws://127.0.0.1:4927/',
   }
 };

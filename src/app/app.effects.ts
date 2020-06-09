6f3f9c8805b6ef0d2c0ce251ea7174644e2e18da
Juraj Selep <jurajselep@gmail.com>
2020-06-09 19:35:34 +0200

    Added mobile styles for logs and endpoints

--- src/app/app.effects.ts
+++ src/app/app.effects.ts
@@ -29,9 +29,9 @@ export class AppEffects {
             let redirectUrl = '';
             if (action.payload.connected) {
                 if (action.payload.ws === false) {
-                    redirectUrl = 'network';
+                    redirectUrl = 'monitoring';
                 } else {
-                    redirectUrl = 'network';
+                    redirectUrl = 'monitoring';
                 }
             } else {
                 redirectUrl = '';

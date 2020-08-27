8ff0be85cff345b23b77e1d51e7077249c1348a6
Luka Stanisic <luka.stanisic@aol.com>
2020-08-27 12:34:17 +0200

    Fixed validators after moving default values to reducer

--- src/app/chain/chain-config/chain-config.component.ts
+++ src/app/chain/chain-config/chain-config.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, Validators } from '@angular/forms';
+import { FormBuilder, Validators, FormGroup } from '@angular/forms';
 import { Store } from '@ngrx/store';
 
 @Component({
@@ -8,44 +8,43 @@ import { Store } from '@ngrx/store';
 	styleUrls: ['./chain-config.component.scss'],
 })
 export class ChainConfigComponent implements OnInit {
-	chainConfig;
-	formDefaults: any;
+	chainConfig: FormGroup;
 
 	constructor(private store: Store<any>, private fb: FormBuilder) {}
 
 	ngOnInit(): void {
 		// create form group
 		this.chainConfig = this.fb.group({
-			preservedCycles: [Validators.required],
+			preservedCycles: ['', [Validators.required]],
 
-			blocksPerCycle: [Validators.required],
-			blocksPerCommit: [Validators.required],
-			blocksPerRollSnap: [Validators.required],
-			blocksPerVotingPeriod: [Validators.required],
-			timeBetweenBlocks: [Validators.required],
-			endorsersPerBlock: [Validators.required],
+			blocksPerCycle: ['', [Validators.required]],
+			blocksPerCommit: ['', [Validators.required]],
+			blocksPerRollSnap: ['', [Validators.required]],
+			blocksPerVotingPeriod: ['', [Validators.required]],
+			timeBetweenBlocks: ['', [Validators.required]],
+			endorsersPerBlock: ['', [Validators.required]],
 
-			hardGasLimitPerOperation: [Validators.required],
-			hardGasLimitPerBlock: [Validators.required],
-			proofOfWorkTreshold: [Validators.required],
-			tokensPerRoll: [Validators.required],
-			michelsonMaxTypeSize: [Validators.required],
-			seedNonceRevelationTip: [Validators.required],
+			hardGasLimitPerOperation: ['', [Validators.required]],
+			hardGasLimitPerBlock: ['', [Validators.required]],
+			proofOfWorkTreshold: ['', [Validators.required]],
+			tokensPerRoll: ['', [Validators.required]],
+			michelsonMaxTypeSize: ['', [Validators.required]],
+			seedNonceRevelationTip: ['', [Validators.required]],
 
-			originationSize: [Validators.required],
-			blockSecurityDeposit: [Validators.required],
-			endorsementSecurityDeposit: [Validators.required],
-			bakingRewardPerEndorsement: [Validators.required],
-			endorsementReward: [Validators.required],
-			costPerByte: [Validators.required],
-			storageLimitPerOperation: [Validators.required],
-			testChainDuration: [Validators.required],
+			originationSize: ['', [Validators.required]],
+			blockSecurityDeposit: ['', [Validators.required]],
+			endorsementSecurityDeposit: ['', [Validators.required]],
+			bakingRewardPerEndorsement: ['', [Validators.required]],
+			endorsementReward: ['', [Validators.required]],
+			costPerByte: ['', [Validators.required]],
+			storageLimitPerOperation: ['', [Validators.required]],
+			testChainDuration: ['', [Validators.required]],
 
-			quorumMin: [Validators.required],
-			quorumMax: [Validators.required],
-			minProposalQuorum: [Validators.required],
-			initialEndorsers: [Validators.required],
-			delayPerMissingEndorsement: [Validators.required],
+			quorumMin: ['', [Validators.required]],
+			quorumMax: ['', [Validators.required]],
+			minProposalQuorum: ['', [Validators.required]],
+			initialEndorsers: ['', [Validators.required]],
+			delayPerMissingEndorsement: ['', [Validators.required]],
 		});
 	}
 }
--- src/app/chain/chain-server/chain-server.component.ts
+++ src/app/chain/chain-server/chain-server.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, Validators } from '@angular/forms';
+import { FormBuilder, Validators, FormGroup } from '@angular/forms';
 import { Store } from '@ngrx/store';
 
 @Component({
@@ -8,43 +8,42 @@ import { Store } from '@ngrx/store';
 	styleUrls: ['./chain-server.component.scss'],
 })
 export class ChainServerComponent implements OnInit {
-	chainServer;
-	formDefaults: any;
+	chainServer: FormGroup;
 
 	constructor(private store: Store<any>, private fb: FormBuilder) {}
 
 	ngOnInit(): void {
 		// create form group
 		this.chainServer = this.fb.group({
-			hostname: [Validators.required],
-			bootstrapDbPath: [Validators.required],
-			maxThreads: [],
-			maxOpenFiles: [],
-			loggingFile: [],
-			loggingFormat: [Validators.required],
-			loggingLevel: [Validators.required],
-			oCamlLogging: [Validators.required],
-			network: [Validators.required],
-			p2pPort: [Validators.required],
-			rpcPort: [Validators.required],
-			webSocketAddress: [Validators.required],
-			monitorPort: [Validators.required],
-			lowerPeerTreshold: [Validators.required],
-			higherPeerTreshold: [Validators.required],
-			peers: [],
-			disableBootstrapDnsLookup: [Validators.required],
-			bootstrapLookupAddresses: [],
-			protocolRunner: [Validators.required],
-			ffiCallsNo: [Validators.required],
-			ffiMaxConn: [Validators.required],
-			ffiConnTimeout: [Validators.required],
-			ffiPoolLifetime: [Validators.required],
-			ffiPoolUnusedTimeout: [Validators.required],
-			mempool: [Validators.required],
-			privateNodeMode: [Validators.required],
-			testChain: [Validators.required],
-			recordingContextActions: [Validators.required],
-			sandboxContextPatching: [Validators.required],
+			hostname: ['', [Validators.required]],
+			bootstrapDbPath: ['', [Validators.required]],
+			maxThreads: [''],
+			maxOpenFiles: [''],
+			loggingFile: [''],
+			loggingFormat: ['', [Validators.required]],
+			loggingLevel: ['', [Validators.required]],
+			oCamlLogging: ['', [Validators.required]],
+			network: ['', [Validators.required]],
+			p2pPort: ['', [Validators.required]],
+			rpcPort: ['', [Validators.required]],
+			webSocketAddress: ['', [Validators.required]],
+			monitorPort: ['', [Validators.required]],
+			lowerPeerTreshold: ['', [Validators.required]],
+			higherPeerTreshold: ['', [Validators.required]],
+			peers: [''],
+			disableBootstrapDnsLookup: ['', [Validators.required]],
+			bootstrapLookupAddresses: [''],
+			protocolRunner: ['', [Validators.required]],
+			ffiCallsNo: ['', [Validators.required]],
+			ffiMaxConn: ['', [Validators.required]],
+			ffiConnTimeout: ['', [Validators.required]],
+			ffiPoolLifetime: ['', [Validators.required]],
+			ffiPoolUnusedTimeout: ['', [Validators.required]],
+			mempool: ['', [Validators.required]],
+			privateNodeMode: ['', [Validators.required]],
+			testChain: ['', [Validators.required]],
+			recordingContextActions: ['', [Validators.required]],
+			sandboxContextPatching: ['', [Validators.required]],
 		});
 	}
 }

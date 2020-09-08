6126e0652f466ca690b8c62eb17f5a5d9c547110
Juraj Selep <jurajselep@gmail.com>
2020-09-08 23:51:52 +0200

    Added parsing for mempool reducer

--- src/app/mempool/mempool-action/mempool-action.reducer.ts
+++ src/app/mempool/mempool-action/mempool-action.reducer.ts
@@ -17,20 +17,64 @@ export function reducer(state = initialState, action) {
 
         case 'MEMPOOL_ACTION_START_SUCCESS':
         case 'MEMPOOL_ACTION_LOAD_SUCCESS': {
-            console.log('[' + action.type + ']', action.payload[0]);
+            // console.log('[' + action.type + ']', action.payload);
             return {
                 ...state,
-                ids: [],
-                // action.payload.length === 0 ? [] : action.payload
-                //     .map(mempoolAction => mempoolAction.id)
-                //     .sort((a, b) => a - b),
-                entities: action.payload,
-                // action.payload.length === 0 ? {} : action.payload
-                //     .reduce((accumulator, mempoolAction) => {
-                //         return {
-                //             ...mempoolAction,
-                //         };
-                //     }, {}),
+                ids: [
+                    ...action.payload.applied
+                        .map(mempoolApplied => mempoolApplied.hash),
+                    ...action.payload.refused
+                        .map(mempoolRefused => mempoolRefused[0]),
+                    ...action.payload.branch_refused
+                        .map(mempoolBranchRefused => mempoolBranchRefused[0]),
+                    ...action.payload.branch_delayed
+                        .map(mempoolBranchDelayed => mempoolBranchDelayed[0]),
+                ],
+                entities: {
+                    ...action.payload.applied
+                        .reduce((accumulator, mempoolApplied) => {
+                            return {
+                                ...accumulator,
+                                [mempoolApplied.hash]: {
+                                    type: 'applied',
+                                    ...mempoolApplied
+                                }
+                            };
+                        }, {}),
+                    ...action.payload.refused
+                        .reduce((accumulator, mempoolRefused) => {
+                            return {
+                                ...accumulator,
+                                [mempoolRefused[0]]: {
+                                    type: 'refused',
+                                    hash: mempoolRefused[0],
+                                    ...mempoolRefused[1]
+                                }
+                            };
+                        }, {}),
+                    ...action.payload.branch_refused
+                        .reduce((accumulator, mempoolBranchRefused) => {
+                            return {
+                                ...accumulator,
+                                [mempoolBranchRefused[0]]: {
+                                    type: 'branchRefused',
+                                    hash: mempoolBranchRefused[0],
+                                    ...mempoolBranchRefused[1]
+                                }
+                            };
+                        }, {}),
+                    ...action.payload.branch_delayed
+                        .reduce((accumulator, mempoolBranchDelayed) => {
+                            return {
+                                ...accumulator,
+                                [mempoolBranchDelayed[0]]: {
+                                    type: 'branchDelayyed',
+                                    hash: mempoolBranchDelayed[0],
+                                    ...mempoolBranchDelayed[1]
+                                }
+                            };
+                        }, {}),
+                },
                 stream: true,
             };
         }

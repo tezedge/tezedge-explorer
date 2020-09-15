3269c5da4e58ebd5e80c57a05839bfa7d6b45553
Juraj Selep <jurajselep@gmail.com>
2020-09-15 22:54:21 +0200

    Fixed sandbox id for docker

--- src/environments/environment.docker.ts
+++ src/environments/environment.docker.ts
@@ -2,7 +2,7 @@ export const environment = {
     production: true,
     api: [
       {
-        id: 'localhost',
+        id: 'sandbox-carthage-tezedge',
         name: 'rust.localhost.node',
         http: 'http://127.0.0.1:18732',
         debugger: 'http://127.0.0.1:17732',

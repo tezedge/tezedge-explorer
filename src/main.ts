66bb28cb6e3e026c008fbc307162013df4a89441
Juraj Selep <jurajselep@gmail.com>
2020-09-20 23:26:55 +0200

    Added profiler for change detection

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,6 @@
-import { enableProdMode } from '@angular/core';
+import { enableProdMode, ApplicationRef } from '@angular/core';
+import { enableDebugTools } from '@angular/platform-browser';
+
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
@@ -7,7 +9,18 @@ import { environment } from './environments/environment';
 
 if (environment.production) {
   enableProdMode();
-}
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+  platformBrowserDynamic().bootstrapModule(AppModule)
+    .catch(err => console.error(err));
+
+} else {
+
+  platformBrowserDynamic().bootstrapModule(AppModule)
+    // debugg with ng.profiler.timeChangeDetection();
+    .then(moduleRef => {
+      const applicationRef = moduleRef.injector.get(ApplicationRef);
+      const componentRef = applicationRef.components[0];
+      enableDebugTools(componentRef);
+    })
+    .catch(err => console.error(err));
+}

9164a7d7230424c6fdd40b0fc7fcbcbbc318eb1e
Juraj Selep <jurajselep@gmail.com>
2020-10-22 11:45:08 +0200

    Added localhost node

--- docker/deploy.sh
+++ docker/deploy.sh
@@ -4,3 +4,6 @@ sudo docker push simplestakingcom/tezedge-explorer:sandbox-v0.6.0
 
 sudo docker build -t simplestakingcom/tezedge-explorer:sandbox-latest .
 sudo docker push simplestakingcom/tezedge-explorer:sandbox-latest
+
+sudo docker build -t simplestakingcom/tezedge-explorer:v0.6.0 . --no-cache=true
+sudo docker push simplestakingcom/tezedge-explorer:v0.6.0
--- src/environments/environment.docker.ts
+++ src/environments/environment.docker.ts
@@ -1,14 +1,20 @@
 export const environment = {
-    production: true,
-    api: [
-      {
-        id: 'sandbox-carthage-tezedge',
-        name: 'rust.localhost.node',
-        http: 'http://127.0.0.1:18732',
-        debugger: 'http://127.0.0.1:17732',
-        ws: 'ws://127.0.0.1:4927',
-      },
-    ],
-    sandbox: 'http://127.0.0.1:3030', 
-  };
-  
+  production: true,
+  api: [
+    {
+      id: 'sandbox-carthage-tezedge',
+      name: 'rust.carthage.node',
+      http: 'http://127.0.0.1:18732',
+      debugger: false,
+      ws: 'ws://127.0.0.1:4927',
+    },
+    {
+      id: 'sandbox-carthage-tezedge',
+      name: 'rust.sandbox.node',
+      http: 'http://127.0.0.1:18732',
+      debugger: 'http://127.0.0.1:17732',
+      ws: 'ws://127.0.0.1:4927',
+    },
+  ],
+  sandbox: 'http://127.0.0.1:3030',
+};

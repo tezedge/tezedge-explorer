ec503145a5e14d6a990c2427ad5f7506c4b6999b
talos.ioan <tjhn.tech@gmail.com>
2020-11-17 13:43:36 +0200

    Create a local environment independent from backend; Create the localhost hode, mock the http server on 3001, mock the debugger server on 3002

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,27 +1,34 @@
 export const environment = {
   production: false,
   api: [
-    // {
-    //   id: 'rust-carthage-tezedge',
-    //   name: 'rust.carthage.tezedge.com',
-    //   http: 'https://carthage.tezedge.com:8752',
-    //   debugger: 'https://carthage.tezedge.com:8753',
-    //   ws: 'wss://carthage.tezedge.com',
-    // },
-    // {
-    //   id: 'ocaml-carthage-tezedge',
-    //   name: 'ocaml.carthage.tezedge.com',
-    //   http: 'https://carthage.tezedge.com:8742',
-    //   debugger: 'https://carthage.tezedge.com:8743',
-    //   ws: false,
-    // },
     {
-      id: 'sandbox-carthage-tezedge',
-      name: 'rust.localhost.node',
-      http: 'http://127.0.0.1:18732',
-      debugger: 'http://127.0.0.1:17732',
-      ws: 'ws://127.0.0.1:4927',
+      id: 'localhost',
+      name: 'localhost',
+      http: 'http://127.0.0.1:3001',
+      debugger: 'http://127.0.0.1:3002',
+      ws: false
     },
+    {
+      id: 'rust-carthage-tezedge',
+      name: 'rust.carthage.tezedge.com',
+      http: 'https://carthage.tezedge.com:8752',
+      debugger: 'https://carthage.tezedge.com:8753',
+      ws: 'wss://carthage.tezedge.com',
+    },
+    {
+      id: 'ocaml-carthage-tezedge',
+      name: 'ocaml.carthage.tezedge.com',
+      http: 'https://carthage.tezedge.com:8742',
+      debugger: 'https://carthage.tezedge.com:8743',
+      ws: false,
+    },
+    // {
+    //   id: 'sandbox-carthage-tezedge',
+    //   name: 'rust.localhost.node',
+    //   http: 'http://127.0.0.1:18732',
+    //   debugger: 'http://127.0.0.1:17732',
+    //   ws: 'ws://127.0.0.1:4927',
+    // },
   ],
-  sandbox: 'http://127.0.0.1:3030', 
+  sandbox: 'http://127.0.0.1:3030',
 };

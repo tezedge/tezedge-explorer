89692b84b6e2368c56b1872232c4d47e23941257
Teofil Jolte <jolteteofil@yahoo.com>
2021-03-03 12:18:26 +0200

    Node switcher fix

--- src/app/network/network-stats/network-stats.reducer.ts
+++ src/app/network/network-stats/network-stats.reducer.ts
@@ -44,7 +44,6 @@ export function reducer(state = initialState, action) {
             return initialState;
 
         case 'NETWORK_STATS_LOAD_SUCCESS': {
-            console.log(234);
             const etaApplicationMinutes =
                 moment().diff(moment(action.payload.timestamp), 'minutes') / state.currentApplicationSpeed;
 

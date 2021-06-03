bab69b2cf04346154ab57716ce45b14b7071c169
Teofil Jolte <jolteteofil@yahoo.com>
2021-06-03 11:43:38 +0300

    Cypress plugin module fix

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -11,11 +11,12 @@
 
 // This function is called when a project is opened or re-opened (e.g. due to
 // the project's config changing)
-require('@cypress/skip-test/support');
+const { support } = require('@cypress/skip-test/support');
 /**
  * @type {Cypress.PluginConfig}
  */
 module.exports = (on, config) => {
   // `on` is used to hook into various events Cypress emits
   // `config` is the resolved Cypress config
+  on('file:preprocessor', support(config));
 }

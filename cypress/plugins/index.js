d7258ad9694f9c8c4a45ffcccc58a4ebe1e3df12
Teofil Jolte <jolteteofil@yahoo.com>
2021-06-03 12:20:05 +0300

    Cypress webpack loader plugin

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -11,12 +11,18 @@
 
 // This function is called when a project is opened or re-opened (e.g. due to
 // the project's config changing)
-const { support } = require('@cypress/skip-test/support');
+// const { support } = require('@cypress/skip-test/support');
+
+const wp = require('@cypress/webpack-preprocessor');
+
 /**
  * @type {Cypress.PluginConfig}
  */
 module.exports = (on, config) => {
   // `on` is used to hook into various events Cypress emits
   // `config` is the resolved Cypress config
-  on('file:preprocessor', support(config));
-}
+  const options = {
+    webpackOptions: require('../webpack.config.js')
+  };
+  on('file:preprocessor', wp(options));
+};

60fa873ee27bfa6491d82ad00de24dd5615a1ba4
Teofil Jolte <jolteteofil@yahoo.com>
2021-06-03 11:28:22 +0300

    Cypress module import plugin

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -11,7 +11,7 @@
 
 // This function is called when a project is opened or re-opened (e.g. due to
 // the project's config changing)
-
+require('@cypress/skip-test/support');
 /**
  * @type {Cypress.PluginConfig}
  */

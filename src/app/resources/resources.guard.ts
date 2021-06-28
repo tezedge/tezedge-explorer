275d65b1945fd1bb854b6684b64be821cd997f45
Teofil Jolte <jolteteofil@yahoo.com>
2021-06-28 17:24:26 +0300

    Bugs fixed, Open API design improvements

--- src/app/resources/resources.guard.ts
+++ src/app/resources/resources.guard.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
 import { Store } from '@ngrx/store';
 import { State } from '../app.reducers';
@@ -12,14 +12,22 @@ import { SettingsNodeApi } from '../shared/types/settings-node/settings-node-api
 })
 export class ResourcesGuard implements CanActivate {
 
-  constructor(private store: Store<State>) { }
+  constructor(private store: Store<State>,
+              private router: Router) { }
 
-  canActivate(
-    route: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
     return this.store.select(selectActiveNode).pipe(
-      map((node: SettingsNodeApi) => node.features.some(f => f.name.includes(route.url[0].path)))
+      map((node: SettingsNodeApi) => {
+        if (node.features.some(f => f.name.includes(route.url[0].path))) {
+          return true;
+        } else if (node.features.some(f => f.name.includes('resources'))) {
+          const url = node.features.find(f => f.name.includes('resources')).name;
+          this.router.navigateByUrl(url);
+          return true;
+        } else {
+          return false;
+        }
+      })
     );
   }
-
 }

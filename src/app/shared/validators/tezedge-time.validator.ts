a6560be4cf81815ed12d20f595cf3a155dcc9c61
Teofil Jolte <jolteteofil@yahoo.com>
2021-07-26 15:09:19 +0300

    Fixed storage virtual scroll

--- src/app/shared/validators/tezedge-time.validator.ts
+++ src/app/shared/validators/tezedge-time.validator.ts
@@ -16,24 +16,6 @@ export class TezedgeTimeValidator {
     return null;
   }
 
-  // static isDate(control: FormControl): { [key: string]: any } {
-  //   const pattern = /^02\/(?:[01]\d|2\d)\/(?:19|20)(?:0[048]|[13579][26]|[2468][048])|(?:0[13578]|10|12)\/(?:[0-2]\d|3[01])\/(?:19|20)\d{2}|(?:0[469]|11)\/(?:[0-2]\d|30)\/(?:19|20)\d{2}|02\/(?:[0-1]\d|2[0-8])\/(?:19|20)\d{2}$/;
-  //
-  //   if (!moment(control.value).format('MM/DD/YYYY').match(pattern)) {
-  //     return { invalidDate: true };
-  //   }
-  //   return null;
-  // }
-  //
-  // static isHour(control: FormControl): { [key: string]: any } {
-  //   const pattern = /^([0-9]|0[0-9]|1[0-9]|2[0-3])$/;
-  //
-  //   if (!control.value.toString().match(pattern)) {
-  //     return { invalidHour: true };
-  //   }
-  //   return null;
-  // }
-
   static getDateFormat(value: string): Date {
     const dateStr = value.slice(value.indexOf(',') + 1);
     const validDate = TezedgeTimeValidator.getValidDate(dateStr);

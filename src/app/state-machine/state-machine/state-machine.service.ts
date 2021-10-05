bbb52178c25fb8a08f1cf59ae51b05559eae2694
directcuteo <37619567+directcuteo@users.noreply.github.com>
2021-10-05 14:20:19 +0300

    Real state machine handshaking diagram (#219)

--- src/app/state-machine/state-machine/state-machine.service.ts
+++ src/app/state-machine/state-machine/state-machine.service.ts
@@ -1,13 +1,13 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable, of } from 'rxjs';
+import { Observable } from 'rxjs';
 import { StateMachineDiagramBlock } from '@shared/types/state-machine/state-machine-diagram-block.type';
 import { StateMachineAction } from '@shared/types/state-machine/state-machine-action.type';
 
 // @ts-ignore
 import * as serverData from './state-machine.json';
-import { delay } from 'rxjs/operators';
 import { StateMachineActionsFilter } from '@shared/types/state-machine/state-machine-actions-filter.type';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -19,7 +19,14 @@ export class StateMachineService {
   constructor(private http: HttpClient) { }
 
   getStateMachineDiagram(): Observable<StateMachineDiagramBlock[]> {
-    return of(diagramStructure).pipe(delay(50));
+    const url = 'http://prod.tezedge.com:18732/dev/shell/automaton/actions_graph';
+    return this.http.get<StateMachineDiagramBlock[]>(url).pipe(map(actions => {
+      actions.forEach(action => {
+        action.type = 'info';
+        action.status = 'completed';
+      });
+      return actions;
+    }));
   }
 
   getStateMachineActions(filter: StateMachineActionsFilter): Observable<StateMachineAction[]> {
@@ -33,156 +40,156 @@ export class StateMachineService {
   }
 }
 
-const diagramStructure: StateMachineDiagramBlock[] = [
-  {
-    type: 'info',
-    title: 'P2P socket authenticate',
-    id: 1,
-    next: [21, 221, 90],
-    status: 'completed',
-  },
-  {
-    type: 'info',
-    title: 'Attempt to write connection message',
-    id: 21,
-    next: [22, 91],
-    status: 'completed',
-  },
-  {
-    type: 'info',
-    title: 'Waiting for a response connection message',
-    id: 22,
-    next: [333, 92],
-    status: 'completed',
-  },
-  {
-    type: 'info',
-    title: 'Connection message received without having sent one attempt to read connection message',
-    id: 221,
-    next: [222, 93],
-    status: 'completed',
-  },
-  {
-    type: 'info',
-    title: 'Attempt to respond',
-    id: 222,
-    next: [333, 94],
-    status: 'completed',
-  },
-  {
-    type: 'info',
-    title: 'Connection messages exchanged successfully',
-    id: 333,
-    next: [3, 95],
-    status: 'completed',
-  },
-  {
-    type: 'info',
-    title: 'Exchange metadata message',
-    id: 3,
-    next: [4, 96],
-    status: 'active',
-  },
-  {
-    type: 'info',
-    title: 'Exchange ack message',
-    id: 4,
-    next: [5, 97],
-    status: 'pending',
-  },
-  {
-    type: 'info',
-    title: 'Exchange metadata chunks',
-    id: 5,
-    next: [6, 98],
-    status: 'pending',
-  },
-  {
-    type: 'info',
-    title: 'Authenticated connection',
-    id: 6,
-    next: [7],
-    status: 'pending',
-  },
-  {
-    type: 'info',
-    title: 'Exchanging ack/nack chunks',
-    id: 7,
-    next: [8],
-    status: 'pending',
-  },
-  {
-    type: 'info',
-    title: 'Connection accepted!',
-    id: 8,
-    next: [],
-    status: 'pending',
-  },
-  {
-    type: 'error',
-    title: 'Unknown Error',
-    id: 90,
-    next: [],
-    status: 'completed',
-  },
-  {
-    type: 'error',
-    title: 'P2p Error',
-    id: 91,
-    next: [],
-    status: 'completed',
-  },
-  {
-    type: 'error',
-    title: 'HTTP Error',
-    id: 92,
-    next: [],
-    status: 'completed',
-  },
-  {
-    type: 'error',
-    title: 'Service Failed',
-    id: 93,
-    next: [],
-    status: 'completed',
-  },
-  {
-    type: 'error',
-    title: 'WS Error',
-    id: 94,
-    next: [],
-    status: 'completed',
-  },
-  {
-    type: 'error',
-    title: 'Connection Error',
-    id: 95,
-    next: [],
-    status: 'completed',
-  },
-  {
-    type: 'error',
-    title: 'Thrown Unknown Error',
-    id: 96,
-    next: [],
-    status: 'completed',
-  },
-  {
-    type: 'error',
-    title: 'Blacklisted',
-    id: 97,
-    next: [],
-    status: 'completed',
-  },
-  {
-    type: 'error',
-    title: 'Kernel Error',
-    id: 98,
-    next: [],
-    status: 'completed',
-  },
-];
-
+// const diagramStructure: StateMachineDiagramBlock[] = [
+//   {
+//     type: 'info',
+//     actionName: 'P2P socket authenticate',
+//     actionId: 1,
+//     nextActions: [21, 221, 90],
+//     status: 'completed',
+//   },
+//   {
+//     type: 'info',
+//     actionName: 'Attempt to write connection message',
+//     actionId: 21,
+//     nextActions: [22, 91],
+//     status: 'completed',
+//   },
+//   {
+//     type: 'info',
+//     actionName: 'Waiting for a response connection message',
+//     actionId: 22,
+//     nextActions: [333, 92],
+//     status: 'completed',
+//   },
+//   {
+//     type: 'info',
+//     actionName: 'Connection message received without having sent one attempt to read connection message',
+//     actionId: 221,
+//     nextActions: [222, 93],
+//     status: 'completed',
+//   },
+//   {
+//     type: 'info',
+//     actionName: 'Attempt to respond',
+//     actionId: 222,
+//     nextActions: [333, 94],
+//     status: 'completed',
+//   },
+//   {
+//     type: 'info',
+//     actionName: 'Connection messages exchanged successfully',
+//     actionId: 333,
+//     nextActions: [3, 95],
+//     status: 'completed',
+//   },
+//   {
+//     type: 'info',
+//     actionName: 'Exchange metadata message',
+//     actionId: 3,
+//     nextActions: [4, 96],
+//     status: 'active',
+//   },
+//   {
+//     type: 'info',
+//     actionName: 'Exchange ack message',
+//     actionId: 4,
+//     nextActions: [5, 97],
+//     status: 'pending',
+//   },
+//   {
+//     type: 'info',
+//     actionName: 'Exchange metadata chunks',
+//     actionId: 5,
+//     nextActions: [6, 98],
+//     status: 'pending',
+//   },
+//   {
+//     type: 'info',
+//     actionName: 'Authenticated connection',
+//     actionId: 6,
+//     nextActions: [7],
+//     status: 'pending',
+//   },
+//   {
+//     type: 'info',
+//     actionName: 'Exchanging ack/nack chunks',
+//     actionId: 7,
+//     nextActions: [8],
+//     status: 'pending',
+//   },
+//   {
+//     type: 'info',
+//     actionName: 'Connection accepted!',
+//     actionId: 8,
+//     nextActions: [],
+//     status: 'pending',
+//   },
+//   {
+//     type: 'error',
+//     actionName: 'Unknown Error',
+//     actionId: 90,
+//     nextActions: [],
+//     status: 'completed',
+//   },
+//   {
+//     type: 'error',
+//     actionName: 'P2p Error',
+//     actionId: 91,
+//     nextActions: [],
+//     status: 'completed',
+//   },
+//   {
+//     type: 'error',
+//     actionName: 'HTTP Error',
+//     actionId: 92,
+//     nextActions: [],
+//     status: 'completed',
+//   },
+//   {
+//     type: 'error',
+//     actionName: 'Service Failed',
+//     actionId: 93,
+//     nextActions: [],
+//     status: 'completed',
+//   },
+//   {
+//     type: 'error',
+//     actionName: 'WS Error',
+//     actionId: 94,
+//     nextActions: [],
+//     status: 'completed',
+//   },
+//   {
+//     type: 'error',
+//     actionName: 'Connection Error',
+//     actionId: 95,
+//     nextActions: [],
+//     status: 'completed',
+//   },
+//   {
+//     type: 'error',
+//     actionName: 'Thrown Unknown Error',
+//     actionId: 96,
+//     nextActions: [],
+//     status: 'completed',
+//   },
+//   {
+//     type: 'error',
+//     actionName: 'Blacklisted',
+//     actionId: 97,
+//     nextActions: [],
+//     status: 'completed',
+//   },
+//   {
+//     type: 'error',
+//     actionName: 'Kernel Error',
+//     actionId: 98,
+//     nextActions: [],
+//     status: 'completed',
+//   },
+// ];
+//
 // const proposals: StateMachineProposal[] = [
 //   {
 //     title: 'INITIATE_CONNECTION',

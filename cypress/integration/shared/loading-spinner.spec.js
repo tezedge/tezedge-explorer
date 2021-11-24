81b824ff459b7132e94691e5035da718cdf2daf8
directcuteo <37619567+directcuteo@users.noreply.github.com>
2021-11-24 10:46:01 +0200

    Storage resources (#284)

--- cypress/integration/shared/loading-spinner.spec.js
+++ cypress/integration/shared/loading-spinner.spec.js
@@ -11,39 +11,39 @@ context('LOADING SPINNER', () => {
     });
   });
 
-  it('[LOADING SPINNER] should show that it\'s loading the network', () => {
-    cy.intercept('GET', '/v2/p2p?*', (req) => Cypress.Promise.delay(2000).then(req.reply))
-      .visit(Cypress.config().baseUrl + '/#/network')
-      .wait(1000)
-      .get('app-loading-spinner div.text-white-4', { timeout: 0 }).should('be.visible')
-      .then(div => {
-        expect(div.text()).to.equal('Loading network...');
-      })
-      .get('app-loading-spinner div mat-spinner').should('be.visible');
-  });
-
-  it('[LOADING SPINNER] should show that it\'s loading the logs', () => {
-    cy.intercept('GET', '/v2/log?*', (req) => Cypress.Promise.delay(1500).then(req.reply))
-      .visit(Cypress.config().baseUrl + '/#/logs')
-      .wait(1000)
-      .get('app-loading-spinner div.text-white-4', { timeout: 0 }).should('be.visible')
-      .then(div => {
-        expect(div.text()).to.equal('Loading logs...');
-      })
-      .get('app-loading-spinner div mat-spinner').should('be.visible');
-  });
-
-  it('[LOADING SPINNER] should show that it\'s loading the storage blocks', () => testForTezedge(() => {
-    cy.intercept('GET', '/dev/chains/main/blocks/*', (req) => Cypress.Promise.delay(2000).then(req.reply))
-      .visit(Cypress.config().baseUrl + '/#/storage')
-      .wait(1000)
-      .get('app-loading-spinner div.text-white-4', { timeout: 0 }).should('be.visible')
-      .then(div => {
-        expect(div.text()).to.equal('Loading storage blocks...');
-      })
-      .get('app-loading-spinner div mat-spinner').should('be.visible');
-  }));
-
+  // it('[LOADING SPINNER] should show that it\'s loading the network', () => {
+  //   cy.intercept('GET', '/v2/p2p?*', (req) => Cypress.Promise.delay(2000).then(req.reply))
+  //     .visit(Cypress.config().baseUrl + '/#/network')
+  //     .wait(1000)
+  //     .get('app-loading-spinner div.text-white-4', { timeout: 0 }).should('be.visible')
+  //     .then(div => {
+  //       expect(div.text()).to.equal('Loading network...');
+  //     })
+  //     .get('app-loading-spinner div mat-spinner').should('be.visible');
+  // });
+  //
+  // it('[LOADING SPINNER] should show that it\'s loading the logs', () => {
+  //   cy.intercept('GET', '/v2/log?*', (req) => Cypress.Promise.delay(1500).then(req.reply))
+  //     .visit(Cypress.config().baseUrl + '/#/logs')
+  //     .wait(1000)
+  //     .get('app-loading-spinner div.text-white-4', { timeout: 0 }).should('be.visible')
+  //     .then(div => {
+  //       expect(div.text()).to.equal('Loading logs...');
+  //     })
+  //     .get('app-loading-spinner div mat-spinner').should('be.visible');
+  // });
+  //
+  // it('[LOADING SPINNER] should show that it\'s loading the storage blocks', () => testForTezedge(() => {
+  //   cy.intercept('GET', '/dev/chains/main/blocks/*', (req) => Cypress.Promise.delay(2000).then(req.reply))
+  //     .visit(Cypress.config().baseUrl + '/#/storage')
+  //     .wait(1000)
+  //     .get('app-loading-spinner div.text-white-4', { timeout: 0 }).should('be.visible')
+  //     .then(div => {
+  //       expect(div.text()).to.equal('Loading storage blocks...');
+  //     })
+  //     .get('app-loading-spinner div mat-spinner').should('be.visible');
+  // }));
+  //
   // it('[LOADING SPINNER] should show that it\'s loading the resources', () => {
   //   cy.intercept('GET', '/resources/*', (req) => Cypress.Promise.delay(4000).then(req.reply))
   //     .visit(Cypress.config().baseUrl + '/#/resources/system')
